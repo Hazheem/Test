a8933cbe4bd6c3179108587d551a8f5a7041a27c
Hazheem <63789654+Hazheem@users.noreply.github.com>
2020-06-11 19:17:24 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <p>"Hello Guys, I am Mohd Khairul Hazheem Bin Mohd Rahimie, the Cloud Computer expert(maybe)!. Please go to my GitHub account https://github.com/Hazheem. </p>
+ </body>
+</html>
